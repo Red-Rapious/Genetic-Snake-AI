a61425c5e429be600441ad2631dec1c4ea6a2175
Red Rapious <red.rapious@gmail.com>
2023-08-02 16:23:13 +0200

    Finished the abstract genetic algorithm impl.

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -12,8 +12,44 @@ pub struct GeneticAlgorithm<S>
     mutation_method: Box<dyn MutationMethod>
 }
 
+impl<S> GeneticAlgorithm<S> 
+    where S: SelectionMethod 
+{
+    pub fn new(
+        selection_method: S,
+        crossover_method: impl CrossoverMethod + 'static,
+        mutation_method: impl MutationMethod + 'static
+        ) -> Self {
+        Self { 
+            selection_method, 
+            crossover_method: Box::new(crossover_method),
+            mutation_method: Box::new(mutation_method)
+        }
+    }
+
+    pub fn evolve<I>(&self, population: &[I]) -> Vec<I>
+        where I: Individual
+    {
+        assert!(!population.is_empty());
+
+        population
+            .iter()
+            .map(|_| {
+                let parent_a = self.selection_method.select(population).genome();
+                let parent_b = self.selection_method.select(population).genome();
+
+                let mut child = self.crossover_method.crossover(parent_a, parent_b);
+
+                self.mutation_method.mutate(&mut child);
+
+                I::create(child)
+            })
+            .collect()
+    }
+}
+
 pub trait Individual {
-    fn genome(&self) -> Vec<f32>;
     fn fitness(&self) -> f32;
-    fn create(chromosome: Vec<f32>) -> Self;
+    fn genome(&self) -> &Vec<f32>;
+    fn create(genom: Vec<f32>) -> Self;
 }
